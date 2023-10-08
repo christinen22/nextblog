9217b1ff69b4d7c5ac924e41a4eec7a9d8fbfdae
christinen22 <christine.elin.louise@gmail.com>
2023-10-08 13:17:40 +0200

    First post and global css and layout

--- nextjs-blog/pages/posts/first-post.tsx
+++ nextjs-blog/pages/posts/first-post.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
-import React from "react";
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import Script from "next/script";
 
 const FirstPost = () => {
   return (
-    <div>
-      <h1>Hejhej</h1>
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      {/*      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload" //load this particular script lazily during browser idle time
+        onLoad={() =>
+          //un any JavaScript code immediately after the script has finished loading.
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      /> */}
+      <h1>First Post</h1>
+      <p>HALLÅ</p>
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href="/">← Back to home</Link>
       </h2>
-    </div>
+    </>
   );
 };
 
